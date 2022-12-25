e0354d0060b039af94a82811eb0123173bc89568
seungjinhan <hanblues@gmail.com>
2022-12-26 00:44:54 +0800

    jwt public

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
-import { APP_INTERCEPTOR } from '@nestjs/core';
+import { APP_GUARD, APP_INTERCEPTOR } from '@nestjs/core';
 import { join } from 'path';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
 import { LoggingInterceptor } from '../config/interceptors/logger.interceptor';
+import { JwtAuthGuard } from './auth/jwt-auth.guard';
+import { RolesGuard } from 'config/roles/roles.guard';
 
 @Module({
   imports: [
@@ -23,6 +25,11 @@ import { LoggingInterceptor } from '../config/interceptors/logger.interceptor';
     {
       provide: APP_INTERCEPTOR,
       useClass: LoggingInterceptor,
+    },
+    { provide: APP_GUARD, useClass: JwtAuthGuard },
+    {
+      provide: APP_GUARD,
+      useClass: RolesGuard,
     },
   ],
 })
--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,18 +1,20 @@
 import { Controller, Get, Post, Req, Request, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LocalAuthGuard } from './local-auth.guard';
-import { JwtAuthGuard } from './jwt-auth.guard';
+import { JwtAuthGuard, Public } from './jwt-auth.guard';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
+  @Public()
   @UseGuards(LocalAuthGuard)
   @Post('login')
   login(@Request() req) {
     return req.user;
   }
 
+  @Public()
   @UseGuards(LocalAuthGuard)
   @Post('token')
   token(@Request() req) {
